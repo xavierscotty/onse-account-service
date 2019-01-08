ae32f62e289e2efc6da8f1bd9a8d5d3784909650
Tom Oram <tom.oram@armakuni.com>
2019-01-08 21:30:27 +0000

    [#162982150] Add GET /accounts/<account_number> endpoint

--- account_service/app.py
+++ account_service/app.py
@@ -4,8 +4,13 @@ from flask import Flask, jsonify
 def create():
     app = Flask(__name__)
 
-    @app.route('/health')
-    def health():
+    @app.route('/health', methods=['GET'])
+    def get_health():
         return jsonify(message='OK')
+
+    @app.route('/accounts/<string:account_number>', methods=['GET'])
+    def get_accounts(account_number):
+        return jsonify({'accountNumber': account_number,
+                        'state': 'active'})
 
     return app
---
+++ tests/test_app.py
@@ -0,0 +1,33 @@
+import pytest
+
+from account_service.app import create
+
+
+@pytest.fixture(scope='function')
+def web_client():
+    return create().test_client()
+
+
+def test_get_health(web_client):
+    response = web_client.get('/health')
+
+    assert response.status_code == 200, \
+        f'Expected status code to be 200; got {response.status_code}'
+    assert response.is_json, \
+        f'Expected content type to be JSON; got "{response.data}'
+    assert response.get_json() == {'message': 'OK'}, \
+        f'Unexpected JSON; got {repr(response.get_json())} '
+
+
+def test_get_accounts_by_number_when_account_exists(web_client):
+    response = web_client.get('/accounts/999999')
+
+    expected_json = {'accountNumber': '999999',
+                     'state': 'active'}
+
+    assert response.status_code == 200, \
+        f'Expected status code to be 200; got {response.status_code}'
+    assert response.is_json, \
+        f'Expected content type to be JSON; got "{response.data}'
+    assert response.get_json() == expected_json, \
+        f'Unexpected JSON; got {repr(response.get_json())} '
