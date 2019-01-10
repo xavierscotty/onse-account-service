489552f0c745533a5d7dcc9031e8687be98081ae
Tom Oram <tom.oram@armakuni.com>
2019-01-10 14:52:53 +0000

    [#163064402] Inject mocks into the app

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import os
 
 from account_service import app
+from account_service.mock.mock_account_repository import MockAccountRepository
+from account_service.mock.mock_customer_client import MockCustomerClient
 
 if __name__ == "__main__":
-    app.create().run(host='0.0.0.0', port=int(os.getenv('PORT', 5001)))
+    app.create(
+        account_repository=MockAccountRepository(),
+        customer_client=MockCustomerClient()
+    ).run(host='0.0.0.0', port=int(os.getenv('PORT', 5001)))
